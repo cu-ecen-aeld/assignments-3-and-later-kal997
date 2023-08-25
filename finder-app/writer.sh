5bf52785ca9c4f6409025476199360642757be12
khaled soliman <khaled.soliman97@gmail.com>
2023-08-25 19:53:02 +0300

    assignment1

---
+++ finder-app/writer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+num_of_input_params=$#
+writefile=$1
+writestr=$2
+
+if [ ${num_of_input_params} -ne 2 ];
+then
+	echo "[ERROR]: invalid input parameters number"
+	exit 1
+
+else
+	
+	mkdir -p .${writefile}
+	if [[ $? -eq 0 ]];
+	then
+		rm -r .${writefile}
+		touch .${writefile}
+		echo "${writestr}" > ./${writefile}
+		exit 0
+	else
+		exit 1
+	fi
+		
+fi

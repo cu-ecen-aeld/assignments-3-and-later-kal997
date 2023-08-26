84cb97612ee47c9b58f58ffcf49154f618cf707b
khaled <khaled.soliman97@gmail.com>
2023-08-26 15:53:17 +0300

    removes tmp

--- full-test.sh
+++ full-test.sh
@@ -3,7 +3,7 @@
 # automated testing using assignment-autotest.  It automates the
 # steps described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
 set -e
-
+rm -r finder-app/tmp
 cd `dirname $0`
 test_dir=`pwd`
 echo "starting test with SKIP_BUILD=\"${SKIP_BUILD}\" and DO_VALIDATE=\"${DO_VALIDATE}\""
